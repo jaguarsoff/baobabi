25f84faad4111ceb6ba38c0c965cba14eb137b3e
jaguarrr <59565481+jaguarsoff@users.noreply.github.com>
2025-11-19 02:53:28 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    bot_token: str
-    admin_id: int | None = None
+    bot_token: 8288012104:AAEy_eFaSjwG8DGBQaRPQGy2cG3pFpCzTqE
+    admin_id: 8254985499
     database_url: str = 'sqlite+aiosqlite:///./poizon.db'
     class Config:
         env_file = '.env'
